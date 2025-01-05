b389a2f5807e3d8fe61358b064c5968c8c819a02
Luiz Henrique Batista <fa34silva@gmail.com>
2025-01-05 14:31:52 -0300

    initial-commit

---
+++ dailyhistory/src/test/java/com/app/dailyhistory/DailyhistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dailyhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyhistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
